b84c86ad6e6e229bd242ffc1f0171642bd8b9ae0
Svetlana <pl.swetik@yandex.ru>
2025-04-29 21:29:02 +0400

    Практика 2 Задача 2а

---
+++ Practice2.Task1a/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Practice2.Task1a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i);
+            }
+        }
+    }
+}
