e4203af1765aab883fd3d6a9e649a4a37c8911a3
Svetlana <pl.swetik@yandex.ru>
2025-04-29 21:24:48 +0400

    Практика 1 Задача 5

---
+++ Practice1.Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const double pi = 3.14;
+        double radius = 15;
+        double square = Math.Pow(radius, 2) * pi;
+        Console.WriteLine($"Площадь круга с радиусом {radius} м равна {square}");
+    }
+}
