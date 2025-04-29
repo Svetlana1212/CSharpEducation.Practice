a3c6554c8a1b3fd5ec4a9a026c5407ac122960fc
Svetlana <pl.swetik@yandex.ru>
2025-04-29 21:23:53 +0400

    Практика 1 Задача 3

---
+++ Practice1.Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите ваше имя:");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}");
+
+        
+    }
+}
