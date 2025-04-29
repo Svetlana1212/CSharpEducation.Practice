d824f981f439b338e0245597a1d2c5596f3a8ffc
Svetlana <pl.swetik@yandex.ru>
2025-04-29 21:24:21 +0400

    Практика 1 Задача 4

---
+++ Practice1.Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите ваше имя, а затем нажмите Enter");
+        string name = Console.ReadLine();
+        Console.WriteLine("Введите ваше отчество, а затем нажмите Enter");
+        string patronymic = Console.ReadLine();
+        Console.WriteLine($"Привет, {name} {patronymic}!");
+    }
+}
