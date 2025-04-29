7abeeede0618d0347940d7cd1f2fe3df86efda66
Svetlana <pl.swetik@yandex.ru>
2025-04-29 21:23:22 +0400

    Практика1 Задача2

---
+++ Practice1.Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int age = 45;
+        string name = "Светлана";
+        double temperature = 35.5;
+        bool femaleGender = true;
+        string gender = (femaleGender) ? "женский" : "мужской";
+        Console.WriteLine($"Имя: {name} Возраст: {age} Температура воздуха: {temperature} Пол: {gender}"); 
+    }
+}
