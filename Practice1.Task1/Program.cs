494c4de8bc0bb3e3ac4e26f995f8718c9b325555
Svetlana <pl.swetik@yandex.ru>
2025-04-29 21:22:50 +0400

    Практика 1 Задача 1

---
+++ Practice1.Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int age;
+        string name;
+        string comanisName;
+        bool logic;
+        float weight;
+        
+    }
+}
