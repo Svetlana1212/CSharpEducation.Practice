05da8625610fb525e0ef814994a44be126aacde9
Svetlana <pl.swetik@yandex.ru>
2025-05-10 20:51:51 +0400

    Задачи с 11 по 17 практика 2

---
+++ Practice2.Task13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+int[,] createAnArray(int n)
+{
+
+    int[,] myArr = new int[n, n];
+    Random ran = new Random();
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            myArr[i, j] = ran.Next(1, 15);
+            //Console.Write("{0}\t", myArr[i, j]);
+        }
+        //Console.WriteLine();
+    }
+    return myArr;
+}
+void arrayOutput(int[,] Array)
+{
+    foreach (int i in Enumerable.Range(0, Array.GetLength(0)))
+    {
+        foreach (int j in Enumerable.Range(0, Array.GetLength(1)))
+        {
+            Console.Write(Array[i, j]);
+        }
+        Console.WriteLine();
+    }
+    
+}
+int num = 5;
+int[,] newArray = createAnArray(num);
+arrayOutput(createAnArray(num));
---
+++ Practice2.Task16/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Practice2.Task16
+{
+    internal class Program
+    {
+        static void ModifyValue(ref int value)
+        {
+            value *= -1;
+        }
+        static void Main(string[] args)
+        {
+            int num = 5;
+            Console.WriteLine("Оригинальное число: " + num);
+
+            ModifyValue(ref num);
+
+            Console.WriteLine("Преобразованное число: " + num);
+            
+        }
+    }
+}
