46eff29abdd6640a31957dbea5d1639f4b8419ef
Svetlana <pl.swetik@yandex.ru>
2025-05-26 21:35:42 +0400

    Практика 4

---
+++ ClassLibraryPerson/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryPerson
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public Person(string myName, int myAge)
+        {
+            Name = myName;
+            Age = myAge;
+        }
+    
+        public static void WriteInfo(Person person)
+        {
+            Console.WriteLine($" Имя - {person.Name}:  возраст - {person.Age}");
+        }
+    }
+}
---
+++ Practice4.Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibraryMathHelper;
+
+namespace Practice4.Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(MathHelper.Summ(5, 6));
+            Console.WriteLine(MathHelper.Sub(15, 6));
+            Console.WriteLine(MathHelper.Mult(5, 6));
+            Console.WriteLine(MathHelper.Div(5, 6));
+        }
+    }
+}
